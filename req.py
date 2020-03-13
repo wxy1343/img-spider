a2732753da31011ca5c82b9f4b2b32d6beb0b9aa
1343890272 <49909001+1343890272@users.noreply.github.com>
2020-03-13 16:20:56 +0800

    Create req.py

---
+++ req.py
@@ -0,0 +1,70 @@
+import requests
+from retrying import retry
+from fake_useragent import UserAgent
+
+
+class Req:
+    def __init__(self):
+        self.req = requests.Session()
+
+    @retry(stop_max_attempt_number=2)
+    def get(self, url='', headers={}, cookies={}):
+        self.url = url
+        self.headers = headers
+        self.cookies = cookies
+        self.headers['user-agent'] = UserAgent().random
+        self.response = self.req.get(url=self.url, headers=self.headers, cookies=cookies)
+        self.response.encoding = 'utf-8'
+        return self.response
+
+    @retry(stop_max_attempt_number=2)
+    def post(self, url='', headers={}, cookies={}, data={}):
+        self.url = url
+        self.headers = headers
+        self.cookies = cookies
+        self.data = data
+        self.headers['user-agent'] = UserAgent().random
+        self.response = self.req.post(url=self.url, headers=self.headers, cookies=cookies, data=data)
+        self.response.encoding = 'utf-8'
+        return self.response
+
+    def get_url(self):
+        return self.url
+
+    def set_url(self, url):
+        self.url = url
+
+    def del_url(self):
+        self.url = None
+
+    def get_headers(self):
+        return self.headers
+
+    def set_headers(self, headers):
+        self.headers = headers
+
+    def del_headers(self):
+        self.headers = None
+
+    def get_cookies(self):
+        return self.req.cookies
+
+    def set_cookies(self, cookies):
+        self.req.cookies = cookies
+
+    def del_cookies(self):
+        self.req.cookies = None
+
+    def get_response(self):
+        return self.response
+
+    def set_response(self, response):
+        self.response = response
+
+    def del_response(self):
+        self.response = None
+
+    _url = property(get_url, set_url, del_url)
+    _headers = property(get_headers, set_headers, del_headers)
+    _cookies = property(get_cookies, set_cookies, del_cookies)
+    _response = property(get_response, set_response, del_response)
