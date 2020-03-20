78a0074b8c6066248027128714d639a69090b680
1343890272 <49909001+1343890272@users.noreply.github.com>
2020-03-21 01:45:29 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,4 +1,5 @@
 from req import *
+import random
 import re
 import time
 import os
@@ -11,6 +12,9 @@ requests.packages.urllib3.disable_warnings()
 sum = 0
 count = 0
 url_list = []
+url_lists = []
+favorites_url_list = []
+favorites_index = 0
 threads = 100
 retry = True
 retry_max = 2
@@ -18,14 +22,20 @@ retry_num = 0
 pool = ThreadPool(threads)
 start_time = 0
 end_time = 0
+page_num = 0
 
 
-def img_download(url):
+def img_download(args):
+    name, url = args
+    if name != '':
+        path = 'img/' + name + '/'
+    else:
+        path = 'img/'
     global count
-    if os.path.exists('img/' + url[-10:]) == True:
-        temp_size = os.path.getsize('img/' + url[-10:])
-    elif os.path.exists('img/' + (url[:-4] + '.png')[-10:]) == True:
-        temp_size = os.path.getsize('img/' + (url[:-4] + '.png')[-10:])
+    if os.path.exists(path + url[-10:]) == True:
+        temp_size = os.path.getsize(path + url[-10:])
+    elif os.path.exists(path + (url[:-4] + '.png')[-10:]) == True:
+        temp_size = os.path.getsize(path + (url[:-4] + '.png')[-10:])
     else:
         temp_size = 0
     headers = {'Range': 'bytes=%d-' % temp_size}
@@ -37,7 +47,7 @@ def img_download(url):
         r = requests.get(url, stream=True, verify=False, headers=headers)
         while r.status_code == 503:
             r = requests.get(url, stream=True, verify=False, headers=headers)
-    with open('img/' + url[-10:], "ab") as f:
+    with open(path + url[-10:], "ab") as f:
         for chunk in r.iter_content(chunk_size=128):
             if chunk:
                 temp_size += len(chunk)
@@ -72,22 +82,34 @@ def init():
         retry_max = cf.GetInt('配置', 'retry_max')
 
 
-def main():
-    # https://wallhaven.cc/search?q=搜索关键词&page=页数
-    # 获取figure标签的data-wallpaper-id属性值
-    # https://w.wallhaven.cc/full/(data-wallpaper-id前两位)/wallhaven-(data-wallpaper-id).jpg
-    init()
-    global sum
-    global pool
-    global start_time
-    global end_time
-    pool = ThreadPool(threads)
-    while True:
-        搜索关键词 = input('请输入搜索关键词：').strip()
-        if 搜索关键词 != '':
-            break
-        else:
-            print('\033[0;37;41m关键词不能为空！\033[0m')
+def parse(url, 开始页数, 页数):
+    for i in range(开始页数, 页数):
+        url = url + 'page=' + str(i + 1)
+        print('正在爬取第%d页...' % (i + 1))
+        req = Req()
+        response = req.get(url)
+        soup = BeautifulSoup(response.text, features='lxml')
+        for j in soup.find_all('figure'):
+            url = 'https://w.wallhaven.cc/full/' + j['data-wallpaper-id'][:2] + '/wallhaven-' + j[
+                'data-wallpaper-id'] + '.jpg'
+            url_list.append(url)
+        print('第%d页爬取成功' % (i + 1))
+
+
+def parse_favorites(url):
+    req = Req()
+    response = req.get(url)
+    soup = BeautifulSoup(response.text, features='lxml')
+    favorites_url_list = []
+    for i, j in enumerate(soup.find('ul', id='collections').find_all('li', {'class': 'collection'})):
+        name = j.find('span', {'class': 'collection-label'}).text
+        print(str(i + 1) + '.' + name)
+        url = j.find('a')['href'] + '?'
+        favorites_url_list.append((name, url))
+    return favorites_url_list
+
+
+def page():
     开始页数 = 0
     while True:
         页数 = input('请输入爬取页数：').strip()
@@ -106,73 +128,178 @@ def main():
             if ('-' in 页数):
                 开始页数 = 页数.split('-')[0]
                 页数 = 页数.split('-')[-1]
-            开始页数 = int(开始页数) - 1
+            if (int(开始页数) > int(页数)) or (int(开始页数) <= 0):
+                print('\033[0;37;41m你的输入非法，请重新输入！\033[0m')
+                continue
             break
         else:
             print('\033[0;37;41m你的输入非法，请重新输入！\033[0m')
+    if 开始页数 != 0:
+        开始页数 = int(开始页数) - 1
     页数 = int(页数)
-    开始页数 = int(开始页数)
-    start_time = time.time()
-    for i in range(开始页数, 页数):
-        url = 'https://wallhaven.cc/search?q=' + 搜索关键词 + '&page=' + str(i + 1)
-        print('正在爬取第%d页...' % (i + 1))
-        req = Req()
-        response = req.get(url)
-        soup = BeautifulSoup(response.text, features='lxml')
-        for j in soup.find_all('figure'):
-            url = 'https://w.wallhaven.cc/full/' + j['data-wallpaper-id'][:2] + '/wallhaven-' + j[
-                'data-wallpaper-id'] + '.jpg'
-            url_list.append(url)
-        print('第%d页爬取成功' % (i + 1))
+    return 开始页数, 页数
 
+
+def Retry(f):
+    global count
+    global retry_num
+    while True:
+        try:
+            f()
+        except (requests.exceptions.ConnectionError, requests.exceptions.ChunkedEncodingError):
+            print('\n\033[0;37;41m远程主机强迫关闭了一个现有的连接。\033[0m')
+            if (retry and (retry_num < retry_max)):
+                retry_num += 1
+                print('第%d次重试' % retry_num)
+                count = 0
+            else:
+                print('\n下载失败')
+                print('感谢您的使用！')
+                input('按Enter键退出')
+                exit()
+        else:
+            break
+
+
+def favorites(name, url):
+    global url_list
+    global url_lists
+    global favorites_url_list
+    global favorites_index
+    global sum
+    global count
+    global retry_num
+    开始页数, 页数 = page()
+    start_time = time.time()
+    parse(url, 开始页数, 页数)
+    url_lists = [(name, url) for url in url_list]
     sum = len(url_list)
     if (sum == 0):
-        print('\033[0;37;41m没有匹配的结果，换个关键词试试吧！\033[0m')
-        main()
+        print('\033[0;37;41m"%s"为空,正在爬取下一个！\033[0m' % name)
+        return
+    if not os.path.exists('img/' + name + '/'):
+        os.mkdir('img/' + name + '/')
     end_time = time.time()
     print('\033[0;37;42m耗时%f秒\033[0m' % (end_time - start_time))
-    if not os.path.exists('img'):
-        os.mkdir('img')
     print('开始下载')
     start_time = time.time()
-    pool.map(img_download, url_list)
+
+    @Retry
+    def download():
+        pool.map(img_download, url_lists)
+
     print('\n下载完成')
     end_time = time.time()
     print('\033[0;37;42m耗时%f秒\033[0m' % (end_time - start_time))
+
+
+def main():
+    # https://wallhaven.cc/search?q=搜索关键词&page=页数
+    # 获取figure标签的data-wallpaper-id属性值
+    # https://w.wallhaven.cc/full/(data-wallpaper-id前两位)/wallhaven-(data-wallpaper-id).jpg
+    init()
+    global sum
+    global pool
+    global start_time
+    global end_time
+    global url_list
+    global favorites_url_list
+    global favorites_index
+    global count
+    global page_num
+    pool = ThreadPool(threads)
+    name = ''
+    while True:
+        print('1.搜索关键词\n2.最新\n3.排行榜\n4.随机\n5.自定义')
+        序号 = input('请输入序号：')
+        if 序号 == '1':
+            while True:
+                搜索关键词 = input('请输入搜索关键词：').strip()
+                if 搜索关键词 != '':
+                    break
+                else:
+                    print('\033[0;37;41m关键词不能为空！\033[0m')
+            url = 'https://wallhaven.cc/search?q=' + 搜索关键词 + '&'
+            name = 搜索关键词
+            break
+        elif 序号 == '2':
+            name = 'latest'
+            url = 'https://wallhaven.cc/latest?'
+            break
+        elif 序号 == '3':
+            name = 'toplist'
+            url = 'https://wallhaven.cc/toplist?'
+            break
+        elif 序号 == '4':
+            seed = input('请输入种子（留空随机种子）：')
+            if (seed.strip() == ''):
+                seed = ''.join(random.sample([chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)], 5))
+                print('你的随机种子是：' + seed)
+            name = seed
+            url = 'https://wallhaven.cc/random?seed=' + seed + '&'
+            break
+        elif 序号 == '5':
+            while True:
+                url = input('请输入自定义网址：').strip()
+                if re.match('^(https://|http://|)wallhaven.cc($|/.*\?|/.*$|/)', url) != None:
+                    if '?' in url:
+                        url = url.split('?')[0] + '?'
+                    elif re.match('.*favorites($|/$)', url):
+                        start_time = time.time()
+                        print('正在爬取收藏中...')
+                        favorites_url_list = parse_favorites(url.rstrip())
+                        end_time = time.time()
+                        print('\033[0;37;42m耗时%f秒\033[0m' % (end_time - start_time))
+                    elif url.rstrip()[-1] == '/':
+                        url = url[:-1] + '?'
+                    else:
+                        url = url.rstrip() + '?'
+                    break
+                print('\033[0;37;41m你输入的网址有误，请重新输入！\033[0m')
+            break
+        else:
+            print('\033[0;37;41m请输入正确的序号！\033[0m')
+            continue
+    开始页数, 页数 = page()
+    if not os.path.exists('img'):
+        os.mkdir('img')
+    if (favorites_url_list != []):
+        favorites_url_list = favorites_url_list[开始页数:页数]
+        favorites_index = 开始页数
+        for i in favorites_url_list:
+            name, url = i
+            print('正在爬取第' + str(favorites_index + 1) + '个：' + name)
+            page_num = 页数
+            if (favorites_index < 页数 + 1):
+                favorites(name, url)
+            url_list = []
+            count = 0
+            favorites_index += 1
+    else:
+        start_time = time.time()
+        parse(url, 开始页数, 页数)
+        sum = len(url_list)
+        if (sum == 0):
+            print('\033[0;37;41m没有匹配的结果，换个关键词试试吧！\033[0m')
+            main()
+        end_time = time.time()
+        print('\033[0;37;42m耗时%f秒\033[0m' % (end_time - start_time))
+        url_lists = [(name, url) for url in url_list]
+        print('开始下载')
+        if not os.path.exists('img/' + name + '/'):
+            os.mkdir('img/' + name + '/')
+        start_time = time.time()
+
+        @Retry
+        def download():
+            pool.map(img_download, url_lists)
+
+        print('\n下载完成')
+        end_time = time.time()
+        print('\033[0;37;42m耗时%f秒\033[0m' % (end_time - start_time))
     print('感谢您的使用！')
     input('按Enter键退出')
 
 
-def Retry():
-    global retry_num
-    global count
-    print('第%d次重试' % retry_num)
-    try:
-        count = 0
-        pool.map(img_download, url_list)
-    except (requests.exceptions.ConnectionError, requests.exceptions.ChunkedEncodingError):
-        print('\n\033[0;37;41m远程主机强迫关闭了一个现有的连接。\033[0m')
-        if (retry_num < retry_max):
-            retry_num += 1
-            Retry()
-        else:
-            print('\n下载失败')
-            print('感谢您的使用！')
-            input('按Enter键退出')
-            exit()
-    print('\n下载完成')
-    end_time = time.time()
-    print('\033[0;37;42m耗时%f秒\033[0m' % (end_time - start_time))
-    return
-
-
 if __name__ == '__main__':
-    try:
-        main()
-    except (requests.exceptions.ConnectionError, requests.exceptions.ChunkedEncodingError):
-        print('\n\033[0;37;41m远程主机强迫关闭了一个现有的连接。\033[0m')
-        if (retry and (retry_num < retry_max)):
-            retry_num += 1
-            Retry()
-        print('感谢您的使用！')
-        input('按Enter键退出')
+    main()
